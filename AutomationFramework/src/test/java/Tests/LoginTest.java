23e3e7e9ec5409e9a73adfcd5b528f86af9162fc
ArpadZk <kisgyorgyza@gmail.com>
2021-08-28 11:39:52 +0300

    Finish Register Test

--- AutomationFramework/src/test/java/Tests/LoginTest.java
+++ AutomationFramework/src/test/java/Tests/LoginTest.java
@@ -12,54 +12,56 @@ public class LoginTest {
     public WebDriver driver;   // o variabila globala - public variabila tip WebDriver
 
 
-    @Test
+    @org.junit.Test
     public void Test(){
 
 
-        System.setProperty("webdriver.chrome.driver", "C:\\automation\\Chrome\\chromedriver.exe");
-             // "cheia", locul unde e driverul pe calculator
+        System.setProperty("webdriver.chrome.driver", "C:\\Automation\\Chrome driver\\chromedriver.exe");// "cheia", locul unde e driverul pe calculator
 
-             // deschizi un browser + accesam un URL
+        // deschizi un browser + accesam un URL
         driver = new ChromeDriver();
         driver.get("http://demo.automationtesting.in/Index.html");
-
-            //Validam pagina de index
-        String expectedindexpage="Index";
-        String actualindexpage=driver.getTitle();
-            //returneaza valoarea din tag ul de titlu
-        Assert.assertEquals("Index page nu a aparut",expectedindexpage,actualindexpage);
-
-
-            //Facem browserul modul maximize (mare).
+        //Facem browserul modul maximize (mare).
         driver.manage().window().maximize();
 
-            // declar elementul "Sign in"
+        //Validam pagina de index
+        String expectedindexpage="Index";
+        String actualindexpage=driver.getTitle();// returneaza valoarea din tag-ul de titlu
+        Assert.assertEquals("Index page nu a aparut",expectedindexpage,actualindexpage);
+
+        // declar elementul "Sign in"
 
         WebElement SigninElement= driver.findElement(By.id("btn1"));
         SigninElement.click();
 
+
         String expectedindexlogin="SignIn";
-        String actualindexlogin=driver.getTitle();
-        Assert.assertEquals("Pagina nu a fost gasit",expectedindexlogin,actualindexlogin);
+        String actualindexlogin= driver.getTitle();
+        Assert.assertEquals("Pagina nu a fost gasita",expectedindexlogin,actualindexlogin);
+
 
         WebElement EmailElement = driver. findElement(By.cssSelector("input[placeholder='E mail']"));
-        String emailvalue = System.currentTimeMillis()+"@gss.com";
+        String emailvalue = "alexandru.alexandruy@gss.com";
         EmailElement.sendKeys(emailvalue);
 
         WebElement passwordElement = driver. findElement(By.cssSelector("input[placeholder='Password']"));
-        String passwordvalue= "parolameasecreta";
+        String passwordvalue= "parolameasecreta ";
         passwordElement.sendKeys(passwordvalue);
 
         WebElement submitElement= driver.findElement(By.id("enterbtn"));
         submitElement.click();
 
-            // validare mesajul de eroare
+        // Validam mesajul de eroare
 
         WebElement errormessage=driver.findElement(By.id("errormsg"));
         String expectederrormessage="Invalid User Name or PassWord";
-        String actualerrormessage = errormessage.getText();
+        String actualerrormessage  = errormessage.getText();
 
         Assert.assertEquals("Mesajul de eroare pentru login nu e bun",expectederrormessage,actualerrormessage);
+
+
+
+
 
     }
 
